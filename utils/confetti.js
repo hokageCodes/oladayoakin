b6a98142ee702f4347a2f3a687ce80bc0e5b5460
hokageCodes <hashsenju94@gmail.com>
2025-08-01 09:02:52 +0100

    g

--- utils/confetti.js
+++ utils/confetti.js
@@ -2,8 +2,8 @@ import confetti from 'canvas-confetti';
 
 export const runConfetti = () => {
   confetti({
-    particleCount: 180,
-    spread: 90,
+    particleCount: 700,
+    spread: 100,
     origin: { y: 0.7 },
     zIndex: 9999,
   });
